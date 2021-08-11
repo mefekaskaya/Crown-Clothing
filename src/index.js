3002070f900aca1ecb9384f5b0814911d8adeab4
mefekaskaya <mefekaskaya@gmail.com>
2021-08-11 22:27:23 +0300

    update to be pwa compliant, added serviceworker, icons and manifest

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
 import App from "./App";
 import { store, persistor } from "./redux/store";
+import * as serviceWorker from './serviceWorkerRegistration.js'
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
@@ -26,3 +27,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
